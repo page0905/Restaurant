1b8c40faf67b74849ebcbcecf319b40747927e8a
page0905 <thtrang14395@gmail.com>
2025-05-27 08:45:46 +0700

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import styles from './Footer.module.css';
-import { FaTwitter, FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
+import React from "react";
+import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
+import { FaTwitter, FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -8,34 +9,66 @@ const Footer = () => {
   return (
     <footer id="footer" className={styles.footer}>
       <div className={styles.borderedBox}>
-      <div className={`${styles.footerTop} container`}>
-  <div className="row">
-    {/* Left Column - Social Links */}
-    <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-start">
-      <div className={` ${styles.footerLinks}`}>
-        <h4>Useful Links</h4>
-        <ul>
-          <li><a href="/hero">Home</a></li>
-          <li><a href="/about">About us</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-    
+        <div className={`${styles.footerTop} container`}>
+          <div className="row">
+            {/* Left Column - Useful Links */}
+            <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-start">
+              <div className={styles.footerLinks}>
+                <h4>Useful Links</h4>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About us</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
 
-    {/* Right Column - Useful Links */}
-    <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-end  align-items-center mb-4 mb-md-0">
-
-      <div className={` ${styles.socialLinks}`}>
-        <a href="#" aria-label="Twitter"><FaTwitter /></a>
-        <a href="#" aria-label="Facebook"><FaFacebook /></a>
-        <a href="#" aria-label="Instagram"><FaInstagram /></a>
-        <a href="#" aria-label="YouTube"><FaYoutube /></a>
+            {/* Right Column - Social Links */}
+            <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-end align-items-center mb-4 mb-md-0">
+              <div className={styles.socialLinks}>
+                <a
+                  href="https://twitter.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Twitter"
+                >
+                  <FaTwitter />
+                </a>
+                <a
+                  href="https://facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Facebook"
+                >
+                  <FaFacebook />
+                </a>
+                <a
+                  href="https://instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Instagram"
+                >
+                  <FaInstagram />
+                </a>
+                <a
+                  href="https://youtube.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="YouTube"
+                >
+                  <FaYoutube />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  </div>
-</div>
     </footer>
   );
 };
